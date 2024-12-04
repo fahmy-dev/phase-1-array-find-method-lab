6a05af151764f7fe2e6f0571035bf93d59af38f3
fahmy-dev <faahmy@proton.me>
2024-12-04 09:49:15 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  
+  function superbowlWin(gameRecords) {
+    function findWin(game) {
+        return game.result === "W";
+    }
+    const winRecord = gameRecords.find(findWin);
+  
+    if (winRecord) {
+        return winRecord.year;
+    } else {
+        return undefined;
+    }
+  }  
